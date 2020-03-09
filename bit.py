709ce93ef68bfccf86454695f8b14dd867b91dc5
Arthur Kockritz <akockritz@crestron.com>
2020-03-09 12:06:39 -0400

    first commit

---
+++ bit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+
+import requests
+import json
+
+#****DEFINITIONS*****************
+ETH = 'ethereum'
+BTC = 'bitcoin'
+LTC = 'litecoin'
+XRP = 'ripple'
+STR = 'stellar'
+#********************************
+
+
+TICKER_API_URL = 'https://api.coinmarketcap.com/v1/ticker/'
+
+def get_latest_crypto_price(crypto):
+	response = requests.get(TICKER_API_URL+crypto)
+	response_json = response.json()
+	return float(response_json[0]['price_usd'])
+
+ETH_USD = get_latest_crypto_price(ETH)
+BTC_USD = get_latest_crypto_price(BTC)
+LTC_USD = get_latest_crypto_price(LTC)
+XRP_USD = get_latest_crypto_price(XRP)
+STR_USD = get_latest_crypto_price(STR)
+
+print "ETH current price: ",ETH_USD
+print "BTC current price: ",BTC_USD
+print "LTC current price: ",LTC_USD
+print "XRP current price: ",XRP_USD
+print "STR current price: ",STR_USD
+
+
